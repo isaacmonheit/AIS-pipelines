22cd4511781242de518ac7da4fa6e1d37c612233
GrogDeFrog <george.knightly@gmail.com>
2024-06-19 14:28:44 -0700

    Added symbolic links to decrease redundancy

--- final3/udp_to_file.sh
+++ final3/udp_to_file.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-
-gst-launch-1.0 -v -e \
-    udpsrc port=5001 caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! \
-    rtph264depay ! h264parse ! mpegtsmux ! filesink location=output.mp4
+scripts/view_udp_stream2.sh
--- final3/view_udp_stream.sh
+++ final3/view_udp_stream.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-# Used in order to view UDP Stream sent from forked_pipeline_demo.sh
-
-### To run this script ###
-#1. Install gstreamer
-#2. Ensure that port number is set correctly
-#4. Run 'chmod +x view_udp_stream.sh'
-#5. Run './view_udp_stream.sh'
-
-gst-launch-1.0 -v \
-    udpsrc port=5000 caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! \
-    rtph264depay ! decodebin ! videoconvert ! autovideosink
+scripts/view_udp_stream.sh
