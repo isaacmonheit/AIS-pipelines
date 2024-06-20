2111bf45daa02b2fc31161e8465e5f8a56b1eb03
GrogDeFrog <george.knightly@gmail.com>
2024-06-20 16:48:33 -0700

    Added shell script pipelines to cpp code... yet works knife not

--- final3/fork.cpp
+++ final3/fork.cpp
@@ -13,7 +13,8 @@ int main (int argc, char *argv[])
     /* Build the pipeline */
     pipeline =
         gst_parse_launch("gst-launch-1.0 -v -e \
-                v4l2src device=/dev/video0 ! video/x-raw, width=640, height=480, framerate=30/1 ! videoconvert ! x264enc ! h264parse ! rtph264pay ! multiudpsink clients=127.0.0.1:5000,127.0.0.1:5001",
+                v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=30/1 ! videoconvert ! x264enc ! \
+                video/x-h264,stream-format=byte-stream,alignment=au ! rtph264pay ! multiudpsink clients=127.0.0.1:5000,127.0.0.1:5001",
                 NULL);
 
     /* Start playing */
