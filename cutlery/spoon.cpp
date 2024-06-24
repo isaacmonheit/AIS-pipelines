680dcfb62dfb290a3e4cc5172d094a9e859e4b22
isaacmonheit <isaacmonheit@gmail.com>
2024-06-24 16:08:03 -0700

    rremoved binaries, updated makefile

--- cutlery/spoon.cpp
+++ cutlery/spoon.cpp
@@ -12,9 +12,9 @@ int main (int argc, char *argv[])
 
     /* Build the pipeline */
     pipeline =
-        gst_parse_launch("gst-launch-1.0 -v \
-                udpsrc port=5000 caps = \"application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96\" ! \
-                rtph264depay ! decodebin ! videoconvert ! autovideosink",
+        gst_parse_launch(
+                "udpsrc port=5000 caps = \"application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96\" ! "
+                "rtph264depay ! decodebin ! videoconvert ! autovideosink",
                 NULL);
 
     /* Start playing */
