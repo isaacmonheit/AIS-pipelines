7e0ea3a33deb96ba57f24eb8541ab278ee7af29d
isaacmonheit <isaacmonheit@gmail.com>
2024-06-24 16:06:28 -0700

    Wrote the beginnings of a class...

--- pipeline-class/Pipeline.cpp
+++ pipeline-class/Pipeline.cpp
@@ -1,8 +1,95 @@
 #include "Pipeline.h"
 
-Pipeline Pipeline::Pipeline()
+Pipeline::Pipeline(Pipeline::PipelineType pt)
 {
-    /* Initialize GStreamer */
+    // Initialize GStreamer
     gst_init (nullptr, nullptr);
 
+    // Feels good to do this shit
+    err = nullptr;
+
+    // Build the pipeline
+    switch (pt)
+    {
+        case Fork:
+            std::cerr << "Squeeze me like the Kennedys\n";
+            pipeline =
+                gst_parse_launch("v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=30/1 ! "
+                        "videoconvert ! x264enc ! video/x-h264,stream-format=byte-stream,alignment=au ! "
+                        "rtph264pay ! multiudpsink clients=127.0.0.1:5000,127.0.0.1:5001",
+                        &err);
+            std::cerr << "Call me the flower twice\n";
+            break;
+        case Knife:
+            pipeline =
+                gst_parse_launch("udpsrc port=5001 caps=\"application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96\" ! "
+                        "rtph264depay ! video/x-h264,stream-format=byte-stream,alignment=au ! mpegtsmux ! filesink location=output.ts", 
+                        &err);
+            break;
+        case Spoon:
+            pipeline =
+                gst_parse_launch("udpsrc port=5000 caps = \"application/x-rtp, media=(string)video, clock-rate=(int)90000, "
+                        "encoding-name=(string)H264, payload=(int)96\" ! rtph264depay ! decodebin ! videoconvert ! autovideosink",
+                        &err);
+            break;
+        default:
+            std::cerr << "You should not be here. (Pipeline type invalid)" << std::endl;
+            exit(EXIT_FAILURE);
+    }
+
+    if (err != NULL)
+    {
+        g_print("Could not construct pipeline: %s\n", err->message);
+        g_error_free(err);
+        exit(-1);
+    }
+
+    // Start actual pipeline
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    
+    // Save pipeline bus
+    bus = gst_element_get_bus(pipeline);
+}
+
+Pipeline::~Pipeline()
+{
+    // Actually stop the pipeline
+    gst_element_set_state (pipeline, GST_STATE_NULL);
+    
+    // Get that end of pipeline message, girl!
+    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
+            static_cast<GstMessageType>(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+    
+    // Parse and print message
+    if (msg != NULL)
+    {
+        GError *err;
+        gchar *debug_info;
+
+        switch (GST_MESSAGE_TYPE (msg))
+        {
+            case GST_MESSAGE_ERROR:
+                gst_message_parse_error (msg, &err, &debug_info);
+                g_printerr("Error received from element %s: %s\n",
+                        GST_OBJECT_NAME (msg->src), err->message);
+                g_printerr("Debugging information: %s\n",
+                        debug_info ? debug_info : "none");
+                g_clear_error(&err);
+                g_free(debug_info);
+                break;
+            case GST_MESSAGE_EOS:
+                g_print("End-Of-Stream reached.\n");
+                break;
+            default:
+                // We should not reach here because we only asked for ERRORs and EOSs
+                g_printerr("Unexpected message received.\n");
+                break;
+        }
+        gst_message_unref(msg);
+    }
+    
+    // Free resources
+    gst_message_unref(msg);
+    gst_object_unref(bus);
+    gst_object_unref(pipeline);
 }
---
+++ pipeline-class/main.cpp
@@ -0,0 +1,11 @@
+#include "Pipeline.h"
+
+int main(int argc, char *argv[])
+{
+    Pipeline pipi(Pipeline::Fork);
+    //Pipeline epo(Pipeline::Spoon);
+    //Pipeline kili(Pipeline::Knife);
+
+    std::cout << "I am famished!" << std::endl;
+    return 0;
+}
