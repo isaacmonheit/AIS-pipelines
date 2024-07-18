1e842e1425f6b4385bdc7de915c212b003295138
GrogDeFrog <george.knightly@gmail.com>
2024-07-18 13:51:58 -0700

    Fix interpipe shell scripts

--- tests/interpipe_scripts/forked_demo_2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gst-launch-1.0 -v -e \
-    v4l2src device=/dev/video0 ! video/x-raw, width=640, height=480, framerate=30/1 ! videoconvert ! x264enc ! h264parse ! interpipesink name=source_pipe \
-    interpipesrc listen-to=source_pipe ! flvmux ! filesink location=output.flv \
-    interpipesrc listen-to=source_pipe ! rtph264pay ! udpsink host=127.0.0.1 port=5000
-    #interpipesrc listen-to=source_pipe ! autovideosink
---
+++ tests/interpipe_scripts/forked_pipeline.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gst-launch-1.0 -v -e \
+    v4l2src device=/dev/video0 ! video/x-raw, width=640, height=480, framerate=30/1 ! videoconvert ! x264enc ! interpipesink name=source_pipe \
+    interpipesrc listen-to=source_pipe ! filesink location=output.264 \
+    interpipesrc listen-to=source_pipe ! rtph264pay ! udpsink host=localhost port=5000
