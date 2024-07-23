350512135e7cb033fd93dc77022821af1e26a5b0
GrogDeFrog <george.knightly@gmail.com>
2024-07-23 14:35:45 -0700

    Reset knork

--- cutlery/knork.cpp
+++ cutlery/knork.cpp
@@ -34,15 +34,11 @@ int main(int argc, char *argv[])
   
     GstElement *interpipesrc1 = gst_element_factory_make("interpipesrc", "interpipesrc1");
     GstElement *vidconvert1 = gst_element_factory_make("videoconvert", "vidconvert1");
-    GstElement *fakesink1 = gst_element_factory_make("fakesink", "fakesink1");
-    GstElement *x264enc1 = gst_element_factory_make("x264enc", "encoder1");
-    GstElement *rtph264pay = gst_element_factory_make("rtph264pay", "rtph264pay");
-    GstElement *udpsink = gst_element_factory_make("udpsink", "udpsink");
+    GstElement *avsink = gst_element_factory_make ("autovideosink", "avsink");
   
     GstElement *interpipesrc2 = gst_element_factory_make("interpipesrc", "interpipesrc2");
-    GstElement *fakesink2 = gst_element_factory_make("fakesink", "fakesink2");
     GstElement *vidconvert2 = gst_element_factory_make("videoconvert", "vidconvert2");
-    GstElement *x264enc2 = gst_element_factory_make("x264enc", "encoder2");
+    GstElement *x264enc = gst_element_factory_make("x264enc", "encoder");
     GstElement *mpegtsmux = gst_element_factory_make("mpegtsmux", "mpegtsmux");
     GstElement *filesink = gst_element_factory_make("filesink", "file-output");
   
@@ -57,7 +53,6 @@ int main(int argc, char *argv[])
                                         "stream-format", G_TYPE_STRING, "byte-stream",
                                         "alignment", G_TYPE_STRING, "au",
                                         NULL);
-
     /* Ensure elements were created */
     if (!pipeline0 || !source || !srccaps || !interpipesink)
     {
@@ -65,13 +60,13 @@ int main(int argc, char *argv[])
       return -1;
     }
   
-    if (!pipeline1 || !interpipesrc1 || !vidconvert1 || !x264enc1 || !rtph264pay || !udpsink)
+    if (!pipeline1 || !interpipesrc1 || !vidconvert1 || !avsink)
     {
       g_printerr("Not all elements in pipeline could be created.\n");
       return -1;
     }
     
-    if (!pipeline2 || ! interpipesrc2 || !vidconvert2 || !x264enc2 || !enccaps || !mpegtsmux || !filesink)
+    if (!pipeline2 || ! interpipesrc2 || !vidconvert2 || !x264enc || !enccaps || !mpegtsmux || !filesink)
     {
       g_printerr("Not all elements in pipeline2 could be created.\n");
       return -1;
@@ -85,9 +80,6 @@ int main(int argc, char *argv[])
                                 NULL);
   
     g_object_set(interpipesrc1, "listen-to", "source_pipe", NULL);
-    g_object_set(udpsink, "host", "127.0.0.1",
-                          "port", 5000,
-                          NULL);
   
     g_object_set(interpipesrc2, "listen-to", "source_pipe",
                                 "accept-events", TRUE,
@@ -97,8 +89,8 @@ int main(int argc, char *argv[])
   
     /* Add elements to the pipeline */
     gst_bin_add_many(GST_BIN(pipeline0), source, interpipesink, NULL);
-    gst_bin_add_many(GST_BIN(pipeline1), interpipesrc1, vidconvert1, fakesink1, x264enc1, rtph264pay, udpsink, NULL);
-    gst_bin_add_many(GST_BIN(pipeline2), interpipesrc2, vidconvert2, fakesink2, x264enc2, mpegtsmux, filesink, NULL);
+    gst_bin_add_many(GST_BIN(pipeline1), interpipesrc1, vidconvert1, avsink, NULL);
+    gst_bin_add_many(GST_BIN(pipeline2), interpipesrc2, vidconvert2, x264enc, mpegtsmux, filesink, NULL);
   
     /* Link elements in pipeline0 */
     if (gst_element_link_filtered(source, interpipesink, srccaps) != TRUE) {
@@ -112,11 +104,12 @@ int main(int argc, char *argv[])
         gst_caps_unref(enccaps);
         return -1;
     }
+  
     gst_caps_unref(srccaps);
   
     /* Link pipeline 1 */
-    if (gst_element_link_many(interpipesrc1, vidconvert1, fakesink1, NULL) != TRUE) {
-        g_printerr("interpipesrc1, vidconvert1, and x264enc1 could not be linked.\n");
+    if (gst_element_link_many(interpipesrc1, vidconvert1, avsink, NULL) != TRUE) {
+        g_printerr("interpipesrc1, vidconvert1, and avsink could not be linked.\n");
   
         gst_object_unref (pipeline0);
         gst_object_unref (pipeline1);
@@ -125,32 +118,10 @@ int main(int argc, char *argv[])
         gst_caps_unref(enccaps);
         return -1;
     }
-
-    //if (gst_element_link_filtered(x264enc1, rtph264pay, enccaps) != TRUE) {
-    //    g_printerr ("x264enc1 and rtph264pay could not be linked.\n");
-  
-    //    gst_object_unref (pipeline0);
-    //    gst_object_unref (pipeline1);
-    //    gst_object_unref (pipeline2);
-  
-    //    gst_caps_unref(enccaps);
-    //    return -1;
-    //}
-
-    //if (gst_element_link(rtph264pay, udpsink) != TRUE) {
-    //    g_printerr ("rtph264pay and udpsink could not be linked.\n");
-  
-    //    gst_object_unref (pipeline0);
-    //    gst_object_unref (pipeline1);
-    //    gst_object_unref (pipeline2);
-  
-    //    gst_caps_unref(enccaps);
-    //    return -1;
-    //}
   
     /* Link pipeline 2 */
-    if (gst_element_link_many(interpipesrc2, vidconvert2, fakesink2, NULL) != TRUE) {
-        g_printerr ("interpipesrc2, vidconvert2, and x264enc2 could not be linked.\n");
+    if (gst_element_link_many(interpipesrc2, vidconvert2, x264enc, NULL) != TRUE) {
+        g_printerr ("interpipesrc2, vidconvert2, and x264enc could not be linked.\n");
   
         gst_object_unref (pipeline0);
         gst_object_unref (pipeline1);
@@ -160,29 +131,27 @@ int main(int argc, char *argv[])
         return -1;
     }
   
-    //if (gst_element_link_filtered(x264enc2, mpegtsmux, enccaps) != TRUE) {
-    //    g_printerr ("x264enc2 and mpegtsmux could not be linked.\n");
+    if (gst_element_link_filtered(x264enc, mpegtsmux, enccaps) != TRUE) {
+        g_printerr ("x264enc and mpegtsmux could not be linked.\n");
   
-    //    gst_object_unref (pipeline0);
-    //    gst_object_unref (pipeline1);
-    //    gst_object_unref (pipeline2);
+        gst_object_unref (pipeline0);
+        gst_object_unref (pipeline1);
+        gst_object_unref (pipeline2);
   
-    //    gst_caps_unref(enccaps);
-    //    return -1;
-    //}
+        gst_caps_unref(enccaps);
+        return -1;
+    }
+  
     gst_caps_unref(enccaps);
   
-    //if (gst_element_link(mpegtsmux, filesink) != TRUE) {
-    //    g_printerr("mpegtsmux and filesink could not be linked.\n");
+    if (gst_element_link(mpegtsmux, filesink) != TRUE) {
+        g_printerr("mpegtsmux and filesink could not be linked.\n");
   
-    //    gst_object_unref (pipeline0);
-    //    gst_object_unref (pipeline1);
-    //    gst_object_unref (pipeline2);
-    //    return -1;
-    //}
-    
-    std::cerr << "\nLINKING COMPLETE\n";
-    std::cerr << "\nSETTING PIPELINES TO PLAYING\n\n";
+        gst_object_unref (pipeline0);
+        gst_object_unref (pipeline1);
+        gst_object_unref (pipeline2);
+        return -1;
+    }
   
     /* Start playing */
     ret = gst_element_set_state (pipeline0, GST_STATE_PLAYING);
@@ -219,15 +188,10 @@ int main(int argc, char *argv[])
         return -1;
     }
   
-    std::cerr << "\nALL ELEMENTS SET TO PLAYING\n";
-    std::cerr << "GETTING BUSES\n\n";
-  
     /* Wait until error or EOS */
     bus0 = gst_element_get_bus(pipeline0);
     bus1 = gst_element_get_bus(pipeline1);
     bus2 = gst_element_get_bus(pipeline2);
-  
-    std::cerr << "\nBUSES GOTTEN\n\n";
   
     /* The order doesn't seem to matter */
     msg0 =
